bd16e3adc6da26b8e68b5f4102115794780a82a8
teatae <teasuzanne@gmail.com>
2022-02-04 11:55:37 -0500

    recursion

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import com.sun.tools.jconsole.JConsoleContext;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+
+	static List<String> all_java_paths = new ArrayList<String>();
 
 	public static Object[] countLine(String path) throws Exception {
 
@@ -53,6 +59,21 @@ public class Main {
 
 	}
 
+	public static void pathRecursion(String path) {
+		File file = new File(path);
+		String[] f_lists = file.list();
+
+		if (f_lists != null) {
+			for(String st : f_lists) {
+				if (st.contains(".java")) {
+					all_java_paths.add(path+"\\"+st);
+				} else {
+					pathRecursion(path+"\\"+st);
+				}
+			}
+		}
+	}
+
 	public static Object[] countPackage(String path) throws Exception {
 
 		int paquet_LOC 	= 0;
@@ -60,32 +81,31 @@ public class Main {
 		Double paquet_DC = 0.0;
 
 		File file = new File(path);
-
 		String[] f_lists = file.list();
 
-		for(String st : f_lists) {
+		pathRecursion(path);
+		System.out.println(all_java_paths);
+		Object[] all_res = new Object[all_java_paths.size()];
+
+		for (int i = 0 ; i < all_java_paths.size(); i++) {
+			String st = all_java_paths.get(i);
 
 			if(st.contains(".java")) {
-
-				Object[] countLine = countLine(path + "\\" + st);
+				Object[] countLine = countLine(st);
 				paquet_LOC += (int)countLine[0];
 				paquet_CLOC += (int)countLine[1];
+
+				Object[] res = new Object[3];
+				paquet_DC = (paquet_CLOC / 1.0) / paquet_LOC;
+				res[0] = paquet_LOC;
+				res[1] = paquet_CLOC;
+				res[2] = paquet_DC;
+
+				all_res[i] = res;
 			}
 
 		}
-
-		Object[] res = new Object[3];
-
-		paquet_DC = (paquet_CLOC / 1.0) / paquet_LOC;
-
-		res[0] = paquet_LOC;
-		res[1] = paquet_CLOC;
-		res[2] = paquet_DC;
-
-
-
-		return res;
-
+		return all_res;
 	}
 
 
@@ -114,13 +134,14 @@ public class Main {
 
 			results = countPackage(path[0]);
 
-			int package_LOC = (int)results[0];
-			int package_CLOC = (int)results[1];
-			double package_DC = (double)results[2];
-
-			System.out.println("paquet_LOC : " + package_LOC);
-			System.out.println("paquet_CLOC : " + package_CLOC);
-			System.out.println("paquet_DC : " + package_DC);
+			for( res : results) {
+				int package_LOC = (int)res[0];
+				int package_CLOC = (int)res[1];
+				double package_DC = (double)res[2];
+				System.out.println("paquet_LOC : " + package_LOC);
+				System.out.println("paquet_CLOC : " + package_CLOC);
+				System.out.println("paquet_DC : " + package_DC);
+			}
 
 		}
 
---
+++ src/hi/Test2.java
@@ -0,0 +1,36 @@
+package hi;
+
+public class Test2 {
+	
+	int i = 0;
+		
+	int j = 0;
+	int k = 0; // with comment
+		//just comment
+		
+		/* long comment 
+		 * 
+		 */
+		
+	int l = 0; /* long comment2
+		
+		*/
+	
+	
+	public void testmethod() {
+		
+		int a = 0;
+		
+		a++;
+		
+		String str = "This " +
+					"is" +
+					"A" +
+					"String";
+		
+	}
+		
+		
+	
+
+	}
